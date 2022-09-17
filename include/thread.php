7e39eac343bb48c6fa4165e70d455d2ca05152ec
nariaki <49401941+japannt@users.noreply.github.com>
2022-09-17 16:50:54 +0200

    Video embedding

--- include/thread.php
+++ include/thread.php
@@ -1,27 +1,21 @@
 <?php
-date_default_timezone_set('UTC');
+	date_default_timezone_set('UTC');
 
-$stmt = $db->prepare("SELECT type FROM posts WHERE (postid=" . $splitRequest[3] . ")");
-$stmt->execute();
-$type = $stmt->fetchAll()[0][0];
+	$stmt = $db->prepare("SELECT type FROM posts WHERE (postid=" . $splitRequest[3] . ")");
+	$stmt->execute();
+	$type = $stmt->fetchAll()[0][0];
 
-if ($type != "post") {
-    http_response_code(404);
-    require_once("$root/error/index.php");
-    require_once("$root/include/footer.php");
-    exit();
-}
+	if ($type != "post") {
+		http_response_code(404);
+		require_once("$root/error/index.php");
+		require_once("$root/include/footer.php");
+		exit();
+	}
 
 ?>
 
-<noscript>
-    <style>.js-only {
-            display: none;
-        }</style>
-</noscript>
-
 <p class="path"><?php
-    print("<a href=\"/$splitRequest[1]\">" . $splitRequest[1] . "</a> - " . $splitRequest[3]); ?></p>
+		print("<a href=\"/$splitRequest[1]\">" . $splitRequest[1] . "</a> - " . $splitRequest[3]); ?></p>
 
 <div class="half-size centered">
     <div class="box">
@@ -33,9 +27,9 @@ if ($type != "post") {
                 <div class="flex-links">
                     <input hidden name="type" value="reply">
                     <input hidden name="replyto" value="<?php
-                    print($splitRequest[3]); ?>">
+						print($splitRequest[3]); ?>">
                     <input hidden name="board" value="<?php
-                    print($splitRequest[1]); ?>">
+						print($splitRequest[1]); ?>">
                     <input type="text" name="name" placeholder="Name" value="Anonymous" required>
                     <textarea name="content" placeholder="Content" required></textarea>
                     <input type="file" name="attachment">
@@ -52,78 +46,92 @@ if ($type != "post") {
 </div>
 
 <?php
-$stmt = $db->prepare("SELECT * FROM posts WHERE (replyto=" . $splitRequest[3] . ") OR (postid=" . $splitRequest[3] . ")");
-$stmt->execute();
-$replies = $stmt->fetchAll();
+	$stmt = $db->prepare("SELECT * FROM posts WHERE (replyto=" . $splitRequest[3] . ") OR (postid=" . $splitRequest[3] . ")");
+	$stmt->execute();
+	$replies = $stmt->fetchAll();
 ?>
 
 <div class="thread">
-    <?php
-    foreach ($replies as $reply) {
+	<?php
+		foreach ($replies as $reply) {
 
-        if ($reply["postid"] == $splitRequest[3]) {
-            $op = " (OP)";
-        } else {
-            $op = "";
-        }
+			if ($reply["postid"] == $splitRequest[3]) {
+				$op = " (OP)";
+			} else {
+				$op = "";
+			}
 
-        ?>
-        <div id="<?php
-        print($reply["postid"]); ?>" class="thread-reply">
-            <div>
-                <span class="green bold"><?php
-                    print($reply["name"]) . $op; ?></span> <?php
-                print(date("d/M/o G:i:s")); ?>
-                <a>No.</a><a href="#<?php
-                print($reply["postid"]); ?>"><?php
-                    print($reply["postid"]); ?></a>
-            </div>
-            <?php
-            if ($reply["mime"] == "image/gif") {
-                print("<span class='mime-hack'>GIF</span>");
-            } else if ($reply["mime"] == "video/webm") {
-                print("<span class='mime-hack'>WEBM</span>");
-            } else if ($reply["mime"] == "video/mp4") {
-                print("<span class='mime-hack'>MP4</span>");
-            } else if ($reply["mime"] == "image/gif") {
-                print("<span class='mime-hack'>GIF</span>");
-            }
-            ?>
+			?>
+            <div id="<?php
+				print($reply["postid"]); ?>" class="thread-reply">
+                <div>
+				<span class="green bold"><?php
+						print($reply["name"]) . $op; ?></span> <?php
+						print(date("d/M/o G:i:s", $reply["timestamp"])); ?>
+                    <a>No.</a><a href="#<?php
+						print($reply["postid"]); ?>"><?php
+							print($reply["postid"]); ?></a>
+                </div>
+				<?php
+					if ($reply["mime"] == "image/gif") {
+						print("<span class='mime-hack'>GIF</span>");
+					} else if ($reply["mime"] == "video/webm") {
+						print("<span class='mime-hack'>WEBM</span>");
+					} else if ($reply["mime"] == "video/mp4") {
+						print("<span class='mime-hack'>MP4</span>");
+					} else if ($reply["mime"] == "image/gif") {
+						print("<span class='mime-hack'>GIF</span>");
+					}
+				?>
 
-            <blockquote>
-                <?php
-                if ($reply["attachmenturl"] != null || $reply["attachmenturl"] != "") {
-                    ?>
-                    <a href="/api/getimg?id=<?= $reply['postid'] ?>" onclick="embed(event)">
-                        <img src="/api/getimg?id=<?= $reply['postid'] ?>&thumb=true" alt="">
-                    </a>
+                <blockquote>
+					<?php
+						if ($reply["attachmenturl"] != null || $reply["attachmenturl"] != "") {
+							?>
+                            <a href="/api/getimg?id=<?= $reply['postid'] ?>" mime="<?php print($reply["mime"]); ?>" onclick="embed(event)">
+								<?php
+									if (str_starts_with($reply["mime"], "image/")) {
+										?>
+                                        <img src="/api/getimg?id=<?= $reply['postid'] ?>&thumb=true" alt="">
+										<?php
+									} else if (str_starts_with($reply["mime"], "video/")) {
+										?>
+                                            <button class="hidden">Close video</button><br class="hidden">
+                                            <img src="/api/getimg?id=<?= $reply['postid'] ?>&thumb=true" alt="">
+                                            <video class="hidden" src="/api/getimg?id=<?= $reply['postid'] ?>" controls></video><br>
+                                            <sup><?php print($reply["filename"] . " " . number_format($reply["size"] / 1024, 2, ".", ".")); ?>KB</sup>
+										<?php
+									}
+								?>
+                            </a>
+							<?php
+						}
+					?>
                     <?php
-                }
-                ?>
+							if ($reply["title"] != null || $reply["title"] != "") {
+								print("<b>" . $reply["title"] . "</b><br>");
+							}
 
-                <pre><?php
-                    if ($reply["title"] != null || $reply["title"] != "") {
-                        print("<b>" . $reply["title"] . "</b><br><br>");
-                    }
+                            ?><pre><?php
 
-                    $txt = $reply["text"];
+							$txt = $reply["text"];
 
-                    $txt = htmlspecialchars($txt);
-                    $txt = preg_replace('/&gt;&gt;(\d*)/', "<a class=\"mention\" href=\"/$splitRequest[1]/thread/$splitRequest[3]#$1\">>>$1</a>", $txt);
-                    $txt = preg_replace("/(^|\n)&gt;.*/", '<span class="green">$0</span>', $txt);
+							$txt = htmlspecialchars($txt);
+							$txt = preg_replace('/&gt;&gt;(\d*)/', "<a class=\"mention\" href=\"/$splitRequest[1]/thread/$splitRequest[3]#$1\">>>$1</a>", $txt);
+							$txt = preg_replace("/(^|\n)&gt;.*/", '<span class="green">$0</span>', $txt);
 
-                    $txt = preg_replace("/\*\*(.+?)\*\*/", '<b>$1</b>', $txt);
-                    $txt = preg_replace("/\*(.+?)\*/", '<i>$1</i>', $txt);
-                    $txt = preg_replace("/`(.+?)`/", '<code>$1</code>', $txt);
+							$txt = preg_replace("/\*\*(.+?)\*\*/", '<b>$1</b>', $txt);
+							$txt = preg_replace("/\*(.+?)\*/", '<i>$1</i>', $txt);
+							$txt = preg_replace("/`(.+?)`/", '<code>$1</code>', $txt);
 
-//                    $txt = str_replace("\n", '<br>');
+                            //$txt = str_replace("\n", '<br>');
 
-                    print($txt); ?></pre>
-            </blockquote>
-        </div>
-        <?php
-    }
-    ?>
+							print($txt); ?></pre>
+                </blockquote>
+            </div>
+			<?php
+		}
+	?>
 </div>
 
 <script src="/js/media-embedder.js"></script>
--- js/media-embedder.js
+++ js/media-embedder.js
@@ -1,15 +1,61 @@
 function embed(e) {
-    let a = e.currentTarget
-    let img = e.currentTarget.children[0]
+	let a = e.currentTarget
+	let url = a.href
+	let mime = a.getAttribute("mime")
 
-    if(img.classList.contains('fullImage')){
-        img.src = a.href + "&thumb=true"
-        img.classList.remove('fullImage')
-    } else {
-        img.src = a.href
-        img.classList.add('fullImage')
-    }
+	if (mime.startsWith("image/")) {
+		let img = a.children[0]
 
-    e.preventDefault()
-    return false
+		if (img.classList.contains('fullImage')) {
+			img.src = a.href + "&thumb=true"
+			img.classList.remove('fullImage')
+		} else {
+			img.src = a.href
+			img.classList.add('fullImage')
+		}
+	} else if (mime.startsWith("video/")) {
+		let button = a.children[0]
+		let breakline = a.children[1]
+		let thumb = a.children[2]
+		let video = a.children[3]
+
+		let mime = a.parentNode.parentNode.children[1]
+
+		if(video.classList.contains("hidden")) {
+			a.href = ""
+
+			thumb.classList.add("hidden")
+
+			breakline.classList.remove("hidden")
+
+			mime.classList.add("hidden")
+
+			video.classList.remove("hidden")
+			video.classList.remove("hidden")
+
+			button.classList.remove("hidden")
+
+			video.play()
+		} else {
+			if (e.target == button) {
+				a.href = url
+
+				thumb.classList.remove("hidden")
+
+				breakline.classList.add("hidden")
+
+				mime.classList.remove("hidden")
+
+				video.classList.add("hidden")
+				video.classList.add("hidden")
+
+				button.classList.add("hidden")
+
+				video.pause()
+			}
+		}
+	}
+
+	e.preventDefault()
+	return false
 }
