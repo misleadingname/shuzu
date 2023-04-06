f8e19c9d7530f843b75753599c8075224bf4be8e
Bartkk <TheBartkk@gmail.com>
2023-04-07 01:13:38 +0200

    Add repliy count to api

--- public/api/posts.php
+++ public/api/posts.php
@@ -9,7 +9,8 @@ $stmt = $db->prepare(<<<SQL
 SELECT postid, timestamp, title, name, text, size, filename, mime, sticky, locked,
        (SELECT COALESCE(MAX(timestamp), p.timestamp)
         FROM posts r
-        WHERE r.postid IN (SELECT rr.postid FROM posts rr WHERE rr.replyto = p.postid ORDER BY rr.postid LIMIT ?)) AS bump
+        WHERE r.postid IN (SELECT rr.postid FROM posts rr WHERE rr.replyto = p.postid ORDER BY rr.postid LIMIT ?)) AS bump,
+        (SELECT COUNT(*) FROM posts r WHERE r.type = 'reply' AND r.replyto = p.postid) AS replies
 FROM posts p
 WHERE boardurl = ?
   AND type = 'post'
