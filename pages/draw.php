6384420dcb8a7b70db47663434fdeeb3a6cd5d46
japannt <49401941+japannt@users.noreply.github.com>
2023-04-18 22:40:56 +0200

    cool stuff

--- pages/draw.php
+++ pages/draw.php
@@ -1,24 +1,72 @@
 <?php
-    die("(¬‿¬)");
+	// die("(¬‿¬)");
+	$stmt = $db->prepare("SELECT url FROM `boards` WHERE `url` = 'draw'");
+	$stmt->execute();
+	$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+	if ($result == null) {
+		http_response_code(404);
+		require_once("include/header.php");
+		require_once("pages/error.php");
+		require_once("include/footer.php");
+		die();
+	}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/draw.css">
-    <script src="/js/canvas.js" defer></script>
-    <title>shuzu - draw</title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/draw.css">
+	<script src="/js/canvas.js" defer></script>
+	<title>shuzu - the canvas</title>
 </head>
 <body>
-    <div class="logo">
-        <pre>The canvas.</pre>
-    </div>
-    <div class="info">
-        <pre>Click to draw.</pre>
-    </div>
-    <canvas></canvas>
+	<div class="container">
+		<div class="logo"></div>
+		<div class="info"></div>
+		<canvas></canvas>
+	</div>
+	<div class="err">
+		<h1>You need a PC</h1>
+		<p>The canvas will only work on PC's.</p>
+		<p><sup>Or a wide enough monitor.</sup></p>
+	</div>
+	<div class="cross">
+		<div></div>
+	</div>
+
+	<div class="toolbox bottom">
+		<div class="colors">
+			<!-- A small color palette that Windows 95 used to have -->
+			<div class="color active" data-color="#000000"></div>
+			<div class="color" data-color="#000080"></div>
+			<div class="color" data-color="#0000C8"></div>
+			<div class="color" data-color="#0000FF"></div>
+			<div class="color" data-color="#006400"></div>
+			<div class="color" data-color="#008000"></div>
+			<div class="color" data-color="#008080"></div>
+			<div class="color" data-color="#00FFFF"></div>
+			<div class="color" data-color="#800000"></div>
+			<div class="color" data-color="#800080"></div>
+			<div class="color" data-color="#808000"></div>
+			<div class="color" data-color="#808080"></div>
+			<div class="color" data-color="#C0C0C0"></div>
+			<div class="color" data-color="#FF0000"></div>
+			<div class="color" data-color="#FF00FF"></div>
+			<div class="color" data-color="#FFFF00"></div>
+			<div class="color" data-color="#FFFFFF"></div>
+		</div>
+	</div>
+
+	<div class="toolbox right column">
+		<h3>Message</h3>
+		<form action="">
+			<textarea name="" id="" cols="30" rows="10"></textarea>
+			<input type="submit" value="Send">
+		</form>
+	</div>
 </body>
 </html>
--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,19 +1,27 @@
 const canvas = document.querySelector('canvas');
+const colors = document.querySelectorAll('.color');
 const ctx = canvas.getContext('2d');
 
 let curzoom = 2;
+let held = false;
 let dragging = false;
 
 let grabX = 0;
 let grabY = 0;
 
-let canvasX = 0;
-let canvasY = 0;
+let canvasX = (window.innerWidth / 2) - (256 / 2)
+let canvasY = (window.innerHeight / 2) - (256 / 2)
+
+applyTransform();
+
+let canvasClickX = 0;
+let canvasClickY = 0;
+
+let lerpOp;
 
 canvas.width = 256;
 canvas.height = 256;
 
-// fill the whole canvas with random pixels color
 for (let i = 0; i < canvas.width; i++) {
 	for (let j = 0; j < canvas.height; j++) {
 		ctx.fillStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
@@ -21,43 +29,124 @@ for (let i = 0; i < canvas.width; i++) {
 	}
 }
 
-// on scroll zoom the canvas using height
+function lerp(a, b, t) {
+	return a + (b - a) * t;
+}
+
+function applyTransform() {
+	canvas.style.left = `${canvasX}px`;
+	canvas.style.top = `${canvasY}px`;
+	canvas.style.transform = `scale(${curzoom})`;
+}
+
+function applyLerpedTransform(targetX, targetY, zoom = 1, frame_timeout = 10, ease = 0.001) {
+	let t = 0;
+
+	clearInterval(lerpOp);
+
+	lerpOp = setInterval(() => {
+		t += ease;
+
+		targetX = (window.innerWidth / 2) - (canvas.width / 2) - (canvasClickX - (canvas.width / 2)) * (zoom);
+		targetY = (window.innerHeight / 2) - (canvas.height / 2) - (canvasClickY - (canvas.height / 2)) * (zoom);
+
+		curzoom = lerp(curzoom, zoom, t);
+		canvasX = lerp(canvasX, targetX , t);
+		canvasY = lerp(canvasY, targetY, t);
+
+		applyTransform();
+
+		if (t >= 0.1) {
+			clearInterval(lerpOp);
+		}
+	}, frame_timeout);
+}
+
 canvas.addEventListener('wheel', (e) => {
+	document.querySelector(".info").classList.add("fade")
+
 	if (e.deltaY < 0) {
-		curzoom += 0.2;
+		curzoom += 0.5;
 	} else {
-		curzoom -= 0.2;
+		curzoom -= 0.5;
 	}
-	if (curzoom > 16) {
-		curzoom = 16;
+	if (curzoom > 64) {
+		curzoom = 64;
 	} else if (curzoom < 2) {
 		curzoom = 2;
 	}
-	canvas.style.transform = `translate(${canvasX}px, ${canvasY}px) scale(${curzoom})`;
+
+	console.log(e)
+
+	// zoom to cursor
+	// e.layerX, e.layerY are the coordinates of the cursor relative to the canvas
+	// canvasClickX, canvasClickY are the coordinates of the cursor relative to the canvas
+	// canvasX, canvasY are the coordinates of the canvas relative to the window
+	// canvas.width, canvas.height are the dimensions of the canvas
+	canvasX += (e.layerX * (1 / curzoom) * curzoom) - (e.layerX * (1 / (curzoom - 0.5)) * (curzoom - 0.5));
+	canvasY += (e.layerY * (1 / curzoom) * curzoom) - (e.layerY * (1 / (curzoom - 0.5)) * (curzoom - 0.5));
+
+	applyTransform();
 });
 
-// dragging the canvas using relative mouse position
 canvas.addEventListener('mousedown', (e) => {
-	canvas.style.cursor = 'grabbing';
+	document.querySelector(".info").classList.add("fade")
 
 	grabX = e.clientX - canvasX;
 	grabY = e.clientY - canvasY;
 
-	dragging = true;
+	canvasClickX = Math.floor(e.layerX * (1 / curzoom) * curzoom);
+	canvasClickY = Math.floor(e.layerY * (1 / curzoom) * curzoom);
+
+	console.log(e.layerX, e.layerY)
+	console.log(canvasClickX, canvasClickY)
+
+	held = true;
 });
+
 
 document.addEventListener('mouseup', (e) => {
-	dragging = false;
 	canvas.style.cursor = "default";
-});
+	held = false;
 
-document.addEventListener('mousemove', (e) => {
+	if (e.target !== canvas) {
+		return;
+	}
+
 	if (dragging) {
-		canvasX = e.clientX - grabX;
-		canvasY = e.clientY - grabY;
+		dragging = false;
+	} else {
+		// color the pixel black
+		ctx.fillStyle = 'black';
+		ctx.fillRect(canvasClickX, canvasClickY, 1, 1);
 
-		canvas.style.transform = `translate(${canvasX}px, ${canvasY}px) scale(${curzoom})`;
+		let x = (window.innerWidth / 2) - (canvas.width / 2) - ((canvasClickX) - (canvas.width / 2)) * (curzoom);
+		let y = (window.innerHeight / 2) - (canvas.height / 2) - ((canvasClickY) - (canvas.height / 2)) * (curzoom);
+
+		applyLerpedTransform(x, y, 64);
 	}
 });
 
-canvas.style.transform = `scale(${curzoom})`;
+document.addEventListener('mousemove', (e) => {
+	if (held) {
+		dragging = true;
+		canvas.style.cursor = 'grabbing';
+
+		canvasX = e.clientX - grabX;
+		canvasY = e.clientY - grabY;
+
+		applyTransform();
+	}
+});
+
+for (let i = 0; i < colors.length; i++) {
+	colors[i].style.backgroundColor = colors[i].dataset.color;
+	colors[i].addEventListener('click', (e) => {
+		colors[i].classList.add('active');
+		for (let j = 0; j < colors.length; j++) {
+			if (j !== i) {
+				colors[j].classList.remove('active');
+			}
+		}
+	});
+}
