aaf03512b63961c73b0a9d49ab6cfc590efc78d2
Bartkk <TheBartkk@gmail.com>
2023-04-18 19:45:27 +0200

    Allow manual webhook pull

--- public/webhook.php
+++ public/webhook.php
@@ -2,18 +2,18 @@
 
 require_once '../config.php';
 
-if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])){
-    die();
-}
+if (isset($_SERVER['HTTP_X_HUB_SIGNATURE'])){
+    list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+    if (!in_array($algo, hash_algos(), TRUE)) {
+        throw new \Exception("Hash algorithm '$algo' is not supported.");
+    }
 
-list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
-if (!in_array($algo, hash_algos(), TRUE)) {
-    throw new \Exception("Hash algorithm '$algo' is not supported.");
-}
-
-$rawPost = file_get_contents('php://input');
-if (!hash_equals($hash, hash_hmac($algo, $rawPost, $ADMIN_PASSWORD))) {
-    throw new \Exception('Hook secret does not match.');
+    $rawPost = file_get_contents('php://input');
+    if (!hash_equals($hash, hash_hmac($algo, $rawPost, $ADMIN_PASSWORD))) {
+        throw new \Exception('Hook secret does not match.');
+    }
+} else {
+    require 'api/admintools/verify.php';
 }
 
 shell_exec("git config --global --add safe.directory '*'");
